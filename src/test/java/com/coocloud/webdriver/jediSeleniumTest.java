4985fe97ffb5f60b4b0ed64804619295ccd012ab
coocloud <apple19903@gmail.com>
2015-11-12 23:02:09 +0200

    added second test

--- src/test/java/com/coocloud/webdriver/jediSeleniumTest.java
+++ src/test/java/com/coocloud/webdriver/jediSeleniumTest.java
@@ -3,6 +3,8 @@ package com.coocloud.webdriver;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 /**
@@ -15,6 +17,19 @@ public class jediSeleniumTest {
         WebDriver driver = new FirefoxDriver();
         driver.navigate().to("http://www.allkpop.com");
         Assert.assertTrue("The title should contain allkpop", driver.getTitle().contains("allkpop"));
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void secondRandomTest(){
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://seleniumhq.org");
+        WebElement downloadTab = driver.findElement(By.id("menu_download"));
+        WebElement downloadLink = downloadTab.findElement(By.tagName("a"));
+        downloadLink.click();
+        Assert.assertTrue("The title should contain Downloads", driver.getTitle().contains("Downloads"));
+        //Assert.assertEquals("Downloads", driver.getTitle());
         driver.close();
         driver.quit();
     }
